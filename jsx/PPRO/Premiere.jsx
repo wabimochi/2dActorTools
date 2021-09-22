f5cdd1cf9edc9b4ee218e4466c8970ed0b214f15
wabimochi <wabimochi@gmail.com>
2021-09-23 08:24:05 +0900

    Revert "クリップを配置するときにオーディオが入らないように変更"
    This reverts commit 09e397814384e96fa3710a0beb0c0cdd43427ed1.

--- jsx/PPRO/Premiere.jsx
+++ jsx/PPRO/Premiere.jsx
@@ -2321,12 +2321,8 @@ function overwriteVideoClip(projectItem, sequence, track, startTime, endFlag, en
 	projectItem.setOverrideFrameRate(1/epsTime);
 	var _startTime = new Time();
 	_startTime.seconds = fixTimeError(startTime, epsTime);
-	projectItem.setInPoint(_startTime.ticks, 1);
-	projectItem.setOutPoint(_endTime.ticks, 1);
-	var audioTime = new Time();
-	audioTime.seconds = 0;
-	projectItem.setInPoint(audioTime.ticks, 2);
-	projectItem.setOutPoint(audioTime.ticks, 2);
+	projectItem.setInPoint(_startTime.ticks, 4);
+	projectItem.setOutPoint(_endTime.ticks, 4);
 	track.overwriteClip(projectItem, startTime);
 	projectItem.setOverrideFrameRate(0);
 }
